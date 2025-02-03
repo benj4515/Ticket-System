0c2a5529bc46693be9038c4d6702833934a4004b
GreenzQe <73607229+GreenzQe@users.noreply.github.com>
2025-02-03 10:41:02 +0100

    Login FXML

--- Ticket_System/src/main/java/dk/easv/ticket_system/HelloController.java
+++ Ticket_System/src/main/java/dk/easv/ticket_system/HelloController.java
@@ -1,11 +1,19 @@
 package dk.easv.ticket_system;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private TextField txtPassword;
 
     @FXML
     protected void onHelloButtonClick() {
