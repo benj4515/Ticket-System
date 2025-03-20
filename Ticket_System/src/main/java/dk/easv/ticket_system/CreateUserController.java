4066837a2bc0f02097f9957c5717e641fcad9202
benj4515 <benjamin.koefoed@gmail.com>
2025-03-20 20:17:21 +0100

    minor design fixes

--- Ticket_System/src/main/java/dk/easv/ticket_system/CreateUserController.java
+++ Ticket_System/src/main/java/dk/easv/ticket_system/CreateUserController.java
@@ -10,6 +10,7 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class CreateUserController {
+    public TextField txtName;
     private String selectedUserType;
     Image icon = new Image(getClass().getResourceAsStream("/Images/EASV.png"));
     @FXML
