b61f707b89cce00125be0c9334bf749931bee770
U1umos <victorsmjensen@gmail.com>
2025-04-02 00:15:52 +0200

    gmail api integration
    gmail api klar til test mangler at "toemail" skal ændres til en rigtig email og mangler en inegration som kalder på send() for at kunne teste det sidste

--- Ticket_System/src/main/java/dk/easv/ticket_system/BLL/Util/TicketManager.java
+++ Ticket_System/src/main/java/dk/easv/ticket_system/BLL/Util/TicketManager.java
@@ -1,17 +1,60 @@
 package dk.easv.ticket_system.BLL.Util;
 
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Message;
 import dk.easv.ticket_system.BE.Ticket;
 import dk.easv.ticket_system.DAL.ITicketDataAccess;
 import dk.easv.ticket_system.DAL.TicketDAO;
+import org.apache.commons.codec.binary.Base64;
 
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.sql.DataSource;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
 import java.util.List;
+import java.util.Properties;
+import java.util.Set;
 
 public class TicketManager {
     private final ITicketDataAccess dataAccess;
-
-    public TicketManager() throws IOException {
+    private final Gmail service;
+    private static final String fromEmailAddress = "eventhubticket@gmail.com";
+    /*
+    * new TicketManager().sendMail("A new message", """
+    * Dear reader,
+    *
+    * Hello world
+    * Best regards,
+    * whoever
+    * """)
+    * */
+    public TicketManager() throws IOException, GeneralSecurityException {
     dataAccess = new TicketDAO();
+
+        NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+        service = new Gmail.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
+                .setApplicationName("Test Mailer")
+                .build();
     }
 
     public Ticket createTicket(Ticket newTicket) throws Exception {
@@ -26,4 +69,65 @@ public class TicketManager {
         dataAccess.deleteTicket(ticketToDelete);
     }
 
+    private static Credential getCredentials(final NetHttpTransport httpTransport, GsonFactory jsonFactory)
+            throws IOException {
+        // Load client secrets.
+
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(TicketManager.class.getResourceAsStream("client_secret_410927174607-63cbdr16t9fia65lds1hbe862cstmp9l.apps.googleusercontent.com.json")));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, jsonFactory, clientSecrets, Set.of(GmailScopes.GMAIL_SEND))
+                .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        //returns an authorized Credential object.
+        return credential;
+    }
+
+    public void send(String subject, String message) throws Exception {
+        //send email to
+
+
+
+        // Create the email content
+        String messageSubject = "Test message";
+        String bodyText = "lorem ipsum.";
+
+        // Encode as MIME message
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage email = new MimeMessage(session);
+        email.setFrom(new InternetAddress(fromEmailAddress));
+        email.addRecipient(javax.mail.Message.RecipientType.TO,
+                new InternetAddress("toEmailAddress"));
+        email.setSubject(subject);
+        email.setText(message);
+
+
+        // Encode and wrap the MIME message into a gmail message
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        email.writeTo(buffer);
+        byte[] rawMessageBytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
+        Message msg = new Message();
+        msg.setRaw(encodedEmail);
+
+        try {
+            // Create the draft message
+            msg = service.users().messages().send("me", msg).execute();
+            System.out.println("Draft id: " + msg.getId());
+            System.out.println(msg.toPrettyString());
+        } catch (GoogleJsonResponseException e) {
+            // TODO - handle error appropriately
+            GoogleJsonError error = e.getDetails();
+            if (error.getCode() == 403) {
+                System.err.println("Unable to create draft: " + e.getDetails());
+            } else {
+                throw e;
+            }
+        }
+    }
 }
