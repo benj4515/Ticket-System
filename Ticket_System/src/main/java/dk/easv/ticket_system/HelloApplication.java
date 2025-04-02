386cfd77df2b497cad63bb622aa23f1886939549
U1umos <victorsmjensen@gmail.com>
2025-04-02 13:14:21 +0200

    can send email with api
    need attachment and checkout integration

--- Ticket_System/src/main/java/dk/easv/ticket_system/HelloApplication.java
+++ Ticket_System/src/main/java/dk/easv/ticket_system/HelloApplication.java
@@ -1,24 +1,37 @@
 package dk.easv.ticket_system;
 
 
+import dk.easv.ticket_system.BLL.Util.EmailHandler;
+import dk.easv.ticket_system.BLL.Util.TicketManager;
 import dk.easv.ticket_system.Controllers.LoginController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 
 public class HelloApplication extends Application {
     private final LoginController loginController = new LoginController();
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
+        new EmailHandler().send("A new message", """
+      Dear reader,
+     
+      Hello world
+      Best regards,
+      EventHub
+       """);
         launch();
 
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception {
         loginController.Start();
+
+
     }
 
 }
